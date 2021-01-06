6a3e2b67d3ad29aabb52a78515dfd5b79d062b96
printco <printco.budget@gmail.com>
2021-01-06 14:59:48 +0700

    one time file upload

--- src/main/java/com/sycapt/HttpServerController.java
+++ src/main/java/com/sycapt/HttpServerController.java
@@ -61,4 +61,52 @@ public class HttpServerController {
 
         return result.toJSONString();
     }
+
+    @RequestMapping( value = "/onetimefileupload"
+            , method = RequestMethod.POST
+            , consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public String onetimeFileUpload(@RequestParam Map<String,String> postdata){
+
+        System.out.println("Post data:");
+        System.out.println(postdata.toString());
+
+        JSONObject result = new JSONObject();
+        JSch jsch = new JSch();
+        try {
+            Session session = jsch.getSession("ftpuser", "localhost", 22);
+            session.setPassword( "ftppassword" );
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            session.setTimeout( 60 * 1000 ); // 60 second
+
+            ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
+            sftpChannel.connect();
+
+            String srcPath = "e:/temp/";
+
+            String destpath = postdata.get("destpath");
+
+            try {
+                String from = srcPath + "*.txt";
+                String to = destpath;
+                System.out.println("Upload from " + from +" to " + to);
+                sftpChannel.put( from, to, ChannelSftp.OVERWRITE);
+                result.put("message", "Upload files success");
+            } catch ( SftpException e ){
+                result.put("message", "Upload failed: " + e.getMessage());
+            }
+
+
+            sftpChannel.disconnect();
+            session.disconnect();
+
+        } catch (JSchException e) {
+            result.put("error", e.getMessage());
+        }
+
+        return result.toJSONString();
+    }
 }
